e062359e1c867e1d1007effe82b993b787fe91ac
Merionard <m.benarab94000@gmail.com>
2022-12-03 17:19:51 +0100

    category-component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
 
+import './category.style.scss';
+import CategoryContainer from './components/category-item/category-container.component';
 const categories = [
   {
-    id:1,
-    title:'Hat',
+    "id": 1,
+    "title": "hats",
+    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
   },
   {
-    id:2,
-    title:'womens'
+    "id": 2,
+    "title": "jackets",
+    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
   },
   {
-    id:3,
-    title:'Mans'
+    "id": 3,
+    "title": "sneakers",
+    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
   },
   {
-    id:4,
-    title:'Jackets'
+    "id": 4,
+    "title": "womens",
+    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
   },
   {
-    id:5,
-    title:'Trucs'
-  },
-
+    "id": 5,
+    "title": "mens",
+    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+  }
 ]
 
-const App = () => {
-  return (
-    <div className="categories-container">
-      {categories.map(({title})=>
-      <div className="categorie-container">
-        <div className="category-body-container">
-          <h2>{title}</h2>
-          <p>Shop now</p>
-        </div>
-      </div>    
-      )}
-    </div>
-  );
-}
+
+const App = () => (<CategoryContainer categories={categories}/>)
+
+
 
 export default App;
