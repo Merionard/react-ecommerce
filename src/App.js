770404c40631a999dfa008a04fce117c463977dc
Merionard <m.benarab94000@gmail.com>
2022-12-10 16:47:31 +0100

    sign-in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './category.style.scss';
 import Home from './components/routes/home/home-component';
 import {Routes,Route} from 'react-router-dom';
 import Navigation from './components/routes/navigation/navigation.component';
+import SignIn from './components/routes/sign-in/sign-in.component';
 
 
 const Shop = () =><h1>SHOP PAGE</h1>;
@@ -12,6 +13,7 @@ const App = () => {return(
     <Route path='/' element={<Navigation/>}>
       <Route index element={<Home/>}/>
       <Route path='shop' element={<Shop/>}/>
+      <Route path='sign-in' element={<SignIn/>}/>
     </Route>
   </Routes>)
 }
---
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,5 @@
+
+
+const SignIn = () => <h1>SIGN IN PAGE</h1>
+
+export default SignIn;
