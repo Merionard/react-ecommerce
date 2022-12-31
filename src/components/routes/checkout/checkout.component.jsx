f99edd8ec28e33e4e04dd72ec50cb6dea9297c04
Merionard <m.benarab94000@gmail.com>
2022-12-31 17:59:14 +0100

    checkout style1

--- src/components/routes/checkout/checkout.component.jsx
+++ src/components/routes/checkout/checkout.component.jsx
@@ -1,15 +1,34 @@
 import { useContext } from "react";
 import { BagContext } from "../../../contexts/bag.context";
 import CheckOutItem from "./checkout.item.component";
+import './checkout.styles.scss';
 
 const Checkout = ()=>{
 
     const {cartItems} = useContext(BagContext)
 
     return(
-        <div>
-        <p>CHECKOUT</p>
+        <div className="checkout-container">
+            <div className="checkout-header">
+                <div className="header-block">
+                <span>Product</span>
+                </div>
+                <div className="header-block">
+                <span>Description</span>
+                </div>
+                <div className="header-block">
+                <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                <span>Price</span>
+                </div>
+                <div className="header-block">
+                <span>Remove</span>
+                </div>
+            
+            </div>
         {cartItems.map(cartItem=><CheckOutItem key={cartItem.id} item={cartItem} />)}
+        <span className="Total">Total: 0</span>
         </div>
     )
 }
--- src/components/routes/checkout/checkout.item.component.jsx
+++ src/components/routes/checkout/checkout.item.component.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { BagContext } from "../../../contexts/bag.context";
+import './checkout.item.styles.scss';
 
 const CheckOutItem = ({ item }) => {
 
@@ -16,11 +17,19 @@ const CheckOutItem = ({ item }) => {
 
 
     return (
-        <div>
-            <img src={imageUrl} alt={name} />
-            <input type='number' value={quantity} onChange={onChangeQuantity} />
-            <a onClick={onDelete} href='#'>X</a>
-            <p>{name}</p>
+        <div className="checkout-item-container">
+        <div className="image-container">
+            <img src={imageUrl} alt={`${name}`} />
+        </div>
+        <span className="name">{name}</span>
+        <span className="quantity">
+            <div className="arrow">&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow">&#10095;</div>
+        </span>
+        <span className="price">{price}</span>
+            
+        <div className="remove-button" onClick={onDelete}>&#10005;</div>
         </div>
 
     )
